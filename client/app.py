c4eb7d14e76543d76ebdc59625decd0eceff3173
bil022 <bil022@ucsd.edu>
2025-05-04 14:03:56 -0700

    host ip

--- client/app.py
+++ client/app.py
@@ -2,8 +2,12 @@ from flask import Flask, jsonify
 import requests
 import time
 import json
-
-server_url = "http://server:5050"
+import os
+import socket
+server_host = os.getenv("SERVER_HOST", "localhost")
+ip = socket.gethostbyname(server_host)
+server_port = os.getenv("SERVER_PORT", "5050")
+server_url = f"http://{ip}:{server_port}"
 
 app = Flask(__name__)
 
@@ -13,7 +17,7 @@ def hello():
     server_message = response.json()  # This is a dictionary
 
     # Convert the dictionary to a string using json.dumps()
-    return jsonify(message="Client call server " + json.dumps(server_message))
+    return jsonify(message="Client call server @" + server_url + ": "  + json.dumps(server_message))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
